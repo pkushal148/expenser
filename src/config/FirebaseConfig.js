e20572f19c499cb93cdf4cfff34252f6c548165c
pkushal148 <pkushal148@gmail.com>
2024-06-19 15:00:45 +0530

    done

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -8,15 +8,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCSzwjXfdAtUSGsAPh9xt_HlD0409xIXFk",
-  authDomain: "expenser-8e5f0.firebaseapp.com",
-  projectId: "expenser-8e5f0",
-  storageBucket: "expenser-8e5f0.appspot.com",
-  messagingSenderId: "755973564642",
-  appId: "1:755973564642:web:4c206e5f489dff4b7e16bb",
-  measurementId: "G-6R940Y99XW",
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
